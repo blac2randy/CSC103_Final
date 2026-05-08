97964ecc19fc0043d3b1160a649dd78c80468330
blac2randy <147206260+blac2randy@users.noreply.github.com>
2026-05-07 21:47:51 -0400

    next step

--- hp.hpp
+++ hp.hpp
@@ -1,11 +1,41 @@
 #pragma once
+#include <iostream>
+#include "hitpointtype.hpp"
+using namespace std;
 
-class hp {
-    public:
+class Health {
+public:
+    // reuturns true if set successfully
+    // Health pool setup, Health crash prevention
+
+    bool setMaxHp(hptype new_max_hp) {
+        if (new_max_hp < 1)
+        return false;
+
+        MaxHP = new_max_hp;
+
+        if (CurrentHP > MaxHP)
+        CurrentHP = MaxHP;
+
+        return true;
+    }
+
+    void takeDamage(hptype damage) {
+        if (damage > CurrentHP){
+        CurrentHP = 0;
+        return;
+        }
+        CurrentHP -= damage;
+    }
+
+    void heal(hptype amount) {
         
+    }
 
-    private:
-        int ShieldHP;
-        int CurrentHP;
-        int MaxHP;
+   
+
+private:
+    hptype ShieldHP;
+    hptype CurrentHP;
+    hptype MaxHP;
 };
